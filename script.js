c546742f371e12bba9187d22d24d551292d198c2
ifritthefiredemon-prog <ifritthefiredemon@gmail.com>
2026-06-26 23:57:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const openButton = document.getElementById("openAssessment");
+const modal = document.getElementById("assessmentModal");
 
+openButton.addEventListener("click", function () {
+    modal.classList.add("show");
+});
