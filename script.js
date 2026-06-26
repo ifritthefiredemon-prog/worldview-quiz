bce61ba51930bb30a8a32eae82460b9c7f5ceec1
ifritthefiredemon-prog <ifritthefiredemon@gmail.com>
2026-06-27 00:08:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+alert("JavaScript Loaded!");
+
 const openButton = document.getElementById("openAssessment");
 const modal = document.getElementById("assessmentModal");
 
 openButton.addEventListener("click", function () {
+    alert("Button clicked!");
     modal.classList.add("show");
 });
