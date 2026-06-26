4b60d139400e7a5cd5705209a17b87f38a2e556b
ifritthefiredemon-prog <ifritthefiredemon@gmail.com>
2026-06-27 01:41:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ const nav = document.querySelector("nav");
 
 let answers = new Array(questions.length).fill(null);
 
+let currentQuestion = 0;
+
+const nextButton = document.getElementById("nextQuestion");
+const prevButton = document.getElementById("prevQuestion");
+
 const nextButton = document.getElementById("nextQuestion");
 const prevButton = document.getElementById("prevQuestion");
 
